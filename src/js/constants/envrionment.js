d99011d97704135e0966ef47002af3bd4006e59f
neondoll <bonni_1998@mail.ru>
2024-08-03 22:30:34 +0300

    first commit

--- src/js/constants/envrionment.js
+++ src/js/constants/envrionment.js
@@ -1,5 +1,5 @@
 /* global process */
 
-const API_ROOT = process.env.API_ROOT;
+const API_ROOT = process.env.API_ROOT || 'https://ahj-homeworks-rxjs-polling-backend-bym5.onrender.com';
 
 export { API_ROOT };
